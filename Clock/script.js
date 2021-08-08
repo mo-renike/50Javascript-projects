8b831189499383e02779a326f8a50f0ebc77081c
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-08 06:33:21 +0100

    changed UI design

--- Clock/script.js
+++ Clock/script.js
@@ -2,14 +2,12 @@ const hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const secondsEl = document.querySelector(".second");
 const toggler = document.querySelector(".toggle");
-const time = document.querySelector(".time");
-const date = document.querySelector(".date");
-const day = document.querySelector(".circle");
- const body = document.querySelector("html");
-
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const body = document.querySelector("html");
 
 // toggler.addEventListener("click", () => {
- 
+
 //   if (body.classList.contains("dark")) {
 //     body.classList.remove("dark");
 //   } else {
@@ -18,12 +16,12 @@ const day = document.querySelector(".circle");
 //   }
 // });
 
-toggler.addEventListener('click', () => {
-    body.classList.toggle('dark')
-    if (body.classList.contains('dark')) {
-        toggler.innerHTML = 'light mode'
-    }
-})
+toggler.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    toggler.innerHTML = "revert";
+  }
+});
 
 const days = [
   "Monday",
@@ -50,7 +48,29 @@ const months = [
 ];
 
 const setTime = () => {
-    const time = new Date()
-    console.log(time.getMonth());
+  const time = new Date();
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const hour12 = hour % 12;
+  const seconds = time.getSeconds();
+  const day = time.getDay();
+  const date = time.getDate();
+  const month = time.getMonth();
+  const amPm = hour >= 12 ? "PM" : "AM"
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hour12, 0, 11, 0, 360)}deg)`;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minute, 0, 59, 0, 360)}deg)`;
+  secondsEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+
+  
+  timeEl.innerHTML = `${hour12} : ${minute < 10 ?  `0${minute}` :minute} ${amPm}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class= 'circle'>${date}</span>`;
+};
+
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
-setTime()
+
+setTime();
+
+setInterval(setTime, 1000)
