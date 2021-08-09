c9166c9248727ec8e2b465dbb93183e8e8e9f7ee
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-09 19:49:16 +0100

    Wrote JS ripple effect  function

--- Button ripple effect/script.js
+++ Button ripple effect/script.js
@@ -1,10 +1,24 @@
 const btns = document.querySelectorAll("#ripple");
 
-btns.forEach(btn => {
-  btn.addEventListener("click", (e) => {
+btns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
     const x = e.clientX;
     const y = e.clientY;
 
-    const 
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+
+    circle.style.top = yInside + "px";
+    circle.style.left = xInside + "px";
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
   });
 });
