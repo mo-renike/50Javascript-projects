c6313ba9943fc6aa295b4e1ba4b88edcdff0ac32
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-09 05:32:38 +0100

    New Project - Button ripple effect

---
+++ Button ripple effect/script.js
@@ -0,0 +1,10 @@
+const btns = document.querySelectorAll("#ripple");
+
+btns.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const 
+  });
+});
