2dc6be1187efe5787d6c2d25f6f4c4f2172678bb
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-16 20:55:35 +0100

    .

--- Drag n Drop/script.js
+++ Drag n Drop/script.js
@@ -1,28 +1,30 @@
 const fill = document.querySelector(".fill");
 const empties = document.querySelectorAll(".empty");
 
-
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
-
+for (let empty of empties){
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
 function dragStart() {
   console.log("drag, start");
-};
-function dragEnd () {
+}
+function dragEnd() {
   console.log("drag, end");
-};
-const dragOver = () => {
+}
+function dragOver () {
   console.log("drag, over");
 };
-const dragEnter = () => {
+function dragEnter () {
   console.log("drag, enter");
 };
-const dragLeave = () => {
+function dragLeave () {
   console.log("drag, leave");
 };
-const dragDrop = () => {
+function dragDrop () {
   console.log("drag, drop");
 };
-
-
