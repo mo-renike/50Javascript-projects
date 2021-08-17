7953392f2e6a9d9f557c0de05a0ff7cd20b6ada8
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-17 09:49:24 +0100

    minor UI fixes

--- Drag n Drop/script.js
+++ Drag n Drop/script.js
@@ -4,27 +4,30 @@ const empties = document.querySelectorAll(".empty");
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
-for (let empty of empties){
-    empty.addEventListener('dragover', dragOver)
-    empty.addEventListener('dragenter', dragEnter)
-    empty.addEventListener('dragleave', dragLeave)
-    empty.addEventListener('drop', dragDrop)
+for (let empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
 }
 function dragStart() {
-  console.log("drag, start");
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisble"), 0);
 }
 function dragEnd() {
-  console.log("drag, end");
+  this.className = "fill";
 }
-function dragOver () {
-  console.log("drag, over");
-};
-function dragEnter () {
-  console.log("drag, enter");
-};
-function dragLeave () {
-  console.log("drag, leave");
-};
-function dragDrop () {
-  console.log("drag, drop");
-};
+function dragOver(e) {
+  e.preventDefault();
+}
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+function dragLeave() {
+  this.className = "empty";
+}
+function dragDrop() {
+  this.className = "empty";
+  this.append(fill);
+}
