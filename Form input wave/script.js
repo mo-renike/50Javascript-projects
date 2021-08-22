0b2dc28ea0882ddbc2cd72107c3bd52b4160273f
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-22 03:12:31 +0100

    refactored script.js

--- Form input wave/script.js
+++ Form input wave/script.js
@@ -1,8 +1,10 @@
-const labels = document.querySelectorAll(".form-controls label");
 
-labels.forEach((label) => {
-  label.innerHTML = label.innerText
-    .split("")
-    .map((leter, idx) => `<span>${letter}</span>`)
-    .join("");
-});
+
+const labels = document.querySelectorAll('.form-control label')
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+        .split('')
+        .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+        .join('')
+})
