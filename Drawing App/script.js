82fb23c83b56e87c1f4475289724c21395dc37a6
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-26 02:45:37 +0100

    created UI

--- Drawing App/script.js
+++ Drawing App/script.js
@@ -1,1 +1,11 @@
-const 
+const add = document.querySelector('#plus')
+const sub = document.querySelector('#minus')
+const figure = document.querySelector('#value')
+const wipe = document.querySelector('#clear')
+const colorBox = document.querySelector('#color')
+
+const ctx = canvas.getContext("2d");
+
+function drawCircle(x, y) {
+    
+}
